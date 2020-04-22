8f53ea04a6f9a574020b80f6be628a026a479bb0
Muhammet Yildiz <muhammetyildiz@gmail.com>
2020-04-22 17:42:16 +0000

    Automatically backed up by Learn

--- test.rb
+++ test.rb
@@ -1,10 +1,12 @@
 
-array=[1,2,3]
-puts "hi"
 
-count = 0
+def square_array(array)
+  count = 0
   square_array = []
 while count < 3
   puts array[count]
   count +=1
 end
+end
+
+square_array [1,2,3]
