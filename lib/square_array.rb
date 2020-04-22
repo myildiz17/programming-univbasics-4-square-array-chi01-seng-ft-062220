bf8a21dbd128798072e434263a44aac76f50b4ba
Muhammet Yildiz <muhammetyildiz@gmail.com>
2020-04-22 17:59:54 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,63 +1,12 @@
 def square_array(array)
-  # your code here
-end
-
-array = [1,2,3,4,5]
-new_array = []
-
-array.length.times do |index|
-  new_array.push(array[index] + 1)
-end
-new_array
- # => [2, 3, 4, 5, 6]
-
- counter = 0
-array = [1,2,3,4,5]
-
-while array[counter] do
-  puts array[counter]
-  counter += 1
-end
-
-
-picnic_ingredients[2] = "Belgian chocolate"
-=> "Belgian chocolate"
-
-artists = ["Hozier", "Ariana Grande", "Usher"]
-next_artist = "Beyonce"
-artists.push(next_artist)
-
-def using_push(array, string)
-  array.push(string)
-end
-
-def output_array_elements(array)
-  counter = 0
-
-  while array[counter] do
-    puts array[counter]
-    counter += 1
-  end
-end
-
-array = ["Spring", "Summer", "Fall", "Winter"]
-array.length
-
-def return_string_array(array)
   count = 0
-  new_array = []
-  while count < array.length do
-    new_array.push(array[count].to_s) # OR new_array << array[count].to_s
-    count += 1
-  end
-  new_array
+  square_array = []
+while count < array.length do
+  square_array.push (array[count]**2)
+  count +=1
+end
+puts square_array
 end
 
-2.6.1 :001 > original_array = [1, 2, 3]
- => [1, 2, 3]
-2.6.1 :002 > return_string_array(original_array)
- => ["1", "2", "3"]
-2.6.1 :003 > original_array
- => ["1", "2", "3"]
-
- 
+square_array [1,2,3]
+square_array [9,10,16,25]
